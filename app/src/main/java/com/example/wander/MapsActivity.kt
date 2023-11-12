898cc7e39fbe089335e327e44a2ebb193cfdfd59
Hsu Yee Thin <hsuyeethin@rebbiz.com>
2023-11-12 14:59:36 +0630

    add poi click on map

--- app/src/main/java/com/example/wander/MapsActivity.kt
+++ app/src/main/java/com/example/wander/MapsActivity.kt
@@ -52,6 +52,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         map.addMarker(MarkerOptions().position(homeLatLong))
 
         setMapLongClick(map)
+        setPoiClick(map)
     }
 
     private fun setMapLongClick(map:GoogleMap) {
@@ -70,6 +71,17 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                     .snippet(snippet)
             )
 
+        }
+    }
+
+    private fun setPoiClick(map: GoogleMap) {
+        map.setOnPoiClickListener { poi ->
+            val poiMarker = map.addMarker(
+                MarkerOptions()
+                    .position(poi.latLng)
+                    .title(poi.name)
+            )
+            poiMarker?.showInfoWindow()
         }
     }
 
